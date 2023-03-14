ef0010f76c720bceaa85e90fd3c1436824ee0d7c
kiike145 <enriquevilas9@gmail.com>
2023-03-14 12:45:00 +0100

    Udating Cart element and applying style

--- src/components/cart/index.js
+++ src/components/cart/index.js
@@ -4,31 +4,52 @@ const Cart = () => {
 
     const productList = JSON.parse(sessionStorage.getItem("productList"));
 
+    let total = 0;
+
+    if (productList !== null) {
+
+        productList.forEach(product => {
+            total += product.precio;
+        });
+    }
+
+    <h3>Total: {total}€</h3>
     return (
         <>
             <h2>Cart</h2>
 
-            {productList === null ?
-                <p className="infoMsg">The cart is empty</p> :
+            <div className="row">
+                {productList === null ?
+                    <p className="infoMsg">The cart is empty</p> :
 
-                productList.map((product) => (
-                    <div key={product.id} className="row">
-                        <div className="col-3 d-flex justify-content-center">
-                            <img src={product.rutaimagen} className="imagenCarro"></img>
-                        </div>
+                    <div className="col-9">
+                        {productList.map((product) => (
+                            <div key={product.id} className="row productRow">
+                                <div className="col-3 d-flex justify-content-center">
+                                    <img src={product.rutaimagen} className="imagenCarro"></img>
+                                </div>
 
-                        <div className="col info">
-                            <h3>{product.nombre}</h3>
-                            <p>{product.descripcion}</p>
-                        </div>
+                                <div className="col info">
+                                    <h3>{product.nombre}</h3>
+                                    <p>{product.descripcion}</p>
+                                </div>
 
-                        <div className="col d-flex flex-col precio">
-                            <h3>{product.precio}€</h3>
-                        </div>
+                                <div className="col d-flex justify-content-end precio">
+                                    <h3>{product.precio}€</h3>
+                                </div>
 
-                        <hr />
+                                <hr />
+                            </div>
+                        ))}
                     </div>
-                ))}
+                }
+
+                {productList === null ? "" :
+                    <div className="col-3 d-flex justify-content-start totalPrecio">
+                        <h3>Total: {total}€</h3>
+                    </div>
+                }
+            </div>
         </>
     );
 };
