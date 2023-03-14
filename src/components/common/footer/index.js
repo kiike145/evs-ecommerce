e9051e85c132346c25b82675f9f51eef70905594
kiike145 <enriquevilas9@gmail.com>
2023-03-14 13:11:57 +0100

    Including icons to footer

--- src/components/common/footer/index.js
+++ src/components/common/footer/index.js
@@ -1,12 +1,16 @@
 import "./footer.css";
 
+import facebook from "../footer/facebook.svg";
+import instagram from "../footer/instagram.svg";
+import tiktok from "../footer/tiktok.svg";
+
 const Footer = () => {
     return (
         <footer>
             <nav className="barraNavegacion footer">
-                <a href="#">Caralibro</a>
-                <a href="#">Insta</a>
-                <a href="#">Tiktok</a>
+                <a href="https://es-es.facebook.com/"><img src={facebook} alt="Icono de facebook"></img></a>
+                <a href="https://www.instagram.com/"><img src={instagram} alt="Icono de instagram"></img></a>
+                <a href="https://www.tiktok.com/es/"><img src={tiktok} alt="Icono de tiktok"></img></a>
             </nav>
         </footer>
     );
