d9f07c2f5d2ebc32915800ca3c44f3de0d636beb
kiike145 <enriquevilas9@gmail.com>
2023-03-14 09:40:49 +0100

    Removing references to "Perfil"

--- EnriqueVilasSerin-BackReact/src/main/java/com/example/demo/model/Usuario.java
+++ EnriqueVilasSerin-BackReact/src/main/java/com/example/demo/model/Usuario.java
@@ -1,16 +1,10 @@
 package com.example.demo.model;
 
-import java.util.LinkedList;
-import java.util.List;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinTable;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -28,11 +22,6 @@ public class Usuario {
 	private String password;
 	private Integer estatus;
 	private String birthday;
-	@ManyToMany(fetch = FetchType.EAGER)
-	@JoinTable(name = "cart",
-			joinColumns = @JoinColumn(name = "idusuario"),
-			inverseJoinColumns = @JoinColumn(name = "idarticulo"))
-	private List<Perfil> perfiles;
 
 	public Integer getId() {
 		return this.id;
@@ -82,27 +71,13 @@ public class Usuario {
 	public void setBirthday(String birthday) {
 		this.birthday = birthday;
 	}
-	public List<Perfil> getPerfiles() {
-		return this.perfiles;
-	}
-	public void setPerfiles(List<Perfil> perfiles) {
-		this.perfiles = perfiles;
-	}
-	
-	// OTROS
-	public void agregar(Perfil tempPerfil) {
-		
-		if (perfiles == null) {
-			perfiles = new LinkedList<Perfil>();
-		}
-		perfiles.add(tempPerfil);
-	}
+
 	
 	@Override
 	public String toString() {
 		return "Usuario [id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", email=" + email
 				+ ", username=" + username + ", password=" + password + ", estatus=" + estatus + ", birthday="
-				+ birthday + ", perfiles=" + perfiles + "]";
+				+ birthday + "]";
 	}
 	
 	
